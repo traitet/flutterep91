6b28860554b89ff218ee681966ece1748af7dff9
Traitet <traitet@gmail.com>
2020-10-13 19:18:41 +0700

    ep91

--- lib/screensep91/Ep911Page.dart
+++ lib/screensep91/Ep911Page.dart
@@ -10,21 +10,14 @@ class _Ep911PageState extends State<Ep911Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('EP81-1: Download and upload Github'),),
+      appBar: AppBar(title: Text('ep91-1: Download and upload Github'),),
       body: ListView(children: <Widget>[
-        Image(image: AssetImage('assets/images/ep81 (1).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (2).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (3).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (4).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (5).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (6).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (7).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (8).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (9).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (10).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (11).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (12).JPG'),),
-        Image(image: AssetImage('assets/images/ep81 (13).JPG'),),                                                                                                          
+        Image(image: AssetImage('assets/images/ep91 (1).JPG'),),
+        Image(image: AssetImage('assets/images/ep91 (2).JPG'),),
+        Image(image: AssetImage('assets/images/ep91 (3).JPG'),),
+        Image(image: AssetImage('assets/images/ep91 (4).JPG'),),
+        Image(image: AssetImage('assets/images/ep91 (5).JPG'),),
+        Image(image: AssetImage('assets/images/ep91 (6).JPG'),),                                                                                                        
       ],),
       
     );
